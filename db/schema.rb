d9c6f8d501d50c0174d442cb8bf0ce2fec49311f
demostudent8 <demostudent8@firstdraft.com>
2019-11-22 20:50:33 +0000

    Generated user accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_202422) do
+ActiveRecord::Schema.define(version: 2019_11_22_204206) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 2019_11_22_202422) do
     t.string "description"
     t.boolean "fifteen_minute_message_sent", default: false
     t.boolean "five_minute_message_sent", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
